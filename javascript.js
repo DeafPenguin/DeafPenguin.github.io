11c658b76eaea982770321dfcac1eefc1d47b6dd
DeafPenguin <victormurilo@gmail.com>
2017-09-27 17:04:17 -0300

    Troca de navtab

--- javascript.js
+++ javascript.js
@@ -71,3 +71,19 @@ window.onscroll = function(e) {
  */
 offSetManager();
 });
+
+/**
+ * New NavBar
+ */
+$(document).ready(function(){
+    $(".dropdown1").hover(            
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+            $(this).toggleClass('open');       
+        }
+    );
+});
